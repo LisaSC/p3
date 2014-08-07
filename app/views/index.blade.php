5941f1ba27b74164506bd286f90f5d36a1f428e6
LisaSC <ink@mit.edu>
2014-08-07 15:22:28 -0400

    debugging

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,15 +1,19 @@
 @extends('_master')
 @section('content')
 	{{ Form::open(array('url'=>'lorem', 'method'=>'POST')) }} 
-		{{ Form::text('num_paragraphs', '5', array('pattern'=>'[1-9]{1}')) }} 
+		Number of paragraphs:<br>
+		{{ Form::text('num_paragraphs', '5', array('size'=>1, 'pattern'=>'[1-9]{1}')) }} 
+		(1-9)<br>
 		{{ Form::submit() }} 
 	{{ Form::close() }} 
 	<br><br>
 	
 	{{ Form::open(array('url'=>'users', 'method'=>'POST')) }} 
-		{{ Form::text('num_users', '5', array('pattern'=>'[0-9]{1,2}')) }} 
-		<br>
-		
+		Number of users:<br>
+		{{ Form::text('num_users', '5', array('size'=>1, 'pattern'=>'[0-9]{1,2}')) }} 
+		(1-99)<br>
+		{{ Form::checkbox('add_birthday', true) }}
+		Include Birthday<br>
 		{{ Form::submit() }} 
 	{{ Form::close() }} 
 
