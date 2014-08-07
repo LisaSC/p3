7c1dc6c49d432fc48d8e1ea8e538e8848c7cc0e8
LisaSC <ink@mit.edu>
2014-08-07 15:05:24 -0400

    debugging

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,13 +1,13 @@
 @extends('_master')
 @section('content')
 	{{ Form::open(array('url'=>'lorem', 'method'=>'POST')) }} 
-		{{ Form::text('num_paragraphs', '5', array('pattern'=>'[1,9]{1}')) }} 
+		{{ Form::text('num_paragraphs', '5', array('pattern'=>'[1-9]{1}')) }} 
 		{{ Form::submit() }} 
 	{{Form::close()}} 
 	<br><br>
 	
 	{{ Form::open(array('url'=>'users', 'method'=>'POST')) }} 
-		{{ Form::text('num_users', '5', array('pattern'=>'[0,9]{1,2}')) }} 
+		{{ Form::text('num_users', '5', array('pattern'=>'[0-9]{1,2}')) }} 
 		<br>
 		
 		{{ Form::submit() }} 
